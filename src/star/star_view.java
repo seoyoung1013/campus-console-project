df1e13c583d0aead728360f3cbe4601e19ca8bd8
ParkJungEun <65385887+Jungeun04@users.noreply.github.com>
2023-05-12 10:38:54 +0900

    구구단

--- src/star/star_view.java
+++ src/star/star_view.java
@@ -1,5 +1,12 @@
 package star;
 
 public class star_view {
-
+	public void selectStar() {
+		System.out.println("별생성기");
+		System.out.print("단수 입력: ");
+	}
+	
+	public void displayStar(String str) {
+		System.out.print(str);
+	}
 }
